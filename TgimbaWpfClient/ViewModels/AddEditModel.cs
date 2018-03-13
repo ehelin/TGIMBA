1908b82e20fd4c72757f43a0661f96da7e0b52d6
Eric Helin <ehelin@felpsis.net>
2018-03-12 23:00:01 -0400

    added add functionality

--- TgimbaWpfClient/ViewModels/AddEditModel.cs
+++ TgimbaWpfClient/ViewModels/AddEditModel.cs
@@ -1,6 +1,70 @@
-﻿namespace TgimbaWpfClient.ViewModels
+﻿using Shared.Interfaces;
+using Shared;
+using CommonServiceCode;
+
+namespace TgimbaWpfClient.ViewModels
 {
     public class AddEditModel : BaseViewModel
     {
+        private ITgimbaService service = null;
+
+        public AddEditModel()
+        {
+            service = new TgimbaService();
+        }
+
+        private string PackageBucketListItem(string name, string date, string category, bool achieved)
+        {
+            string[] newBucketListItem = new string[6];
+            string singleLineBucketListItem = "";
+
+            newBucketListItem[0] = name;
+            newBucketListItem[1] = date;
+            newBucketListItem[2] = category;
+
+            if (achieved)
+            {
+                newBucketListItem[3] = "1";
+            } 
+            else
+            {
+                newBucketListItem[3] = "0";
+            }
+
+            newBucketListItem[4] = "0";
+            newBucketListItem[5] = BucketListModel.userName;
+
+            foreach (string bucketListItemEntry in newBucketListItem)
+            {
+                singleLineBucketListItem += "," + bucketListItemEntry;
+            }
+
+            return singleLineBucketListItem;
+        }
+
+        public bool AddBucketListItem(string name, string date, string category, bool achieved)
+        {
+            if (!BaseViewModel.IsLoggedIn())
+            {
+                return false; 
+            }
+
+            string newBucketListItem = PackageBucketListItem(name, date, category, achieved);
+            
+            string base64NewBucketListItem = Utilities.EncodeClientBase64String(newBucketListItem);
+            string base64UserName = Utilities.EncodeClientBase64String(BaseViewModel.userName);
+            string base64Token = Utilities.EncodeClientBase64String(BaseViewModel.token);
+
+            var result = service.UpsertBucketListItem(base64NewBucketListItem, base64UserName, base64Token);
+
+            if (result != null && result.Length == 1 && result[0] == "TokenValid")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- TgimbaWpfClient/Views/AddEditView.xaml.cs
+++ TgimbaWpfClient/Views/AddEditView.xaml.cs
@@ -1,17 +1,53 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using TgimbaWpfClient.ViewModels;
 
 namespace TgimbaWpfClient.Views
 {
     public partial class AddEditView : UserControl
     {
+        private AddEditModel addEditModel = null;
+
         public AddEditView()
         {
             InitializeComponent();
+
+            addEditModel = new AddEditModel();
+            clearFields();
+
+            tbDate.Text = DateTime.Now.ToString("MM/dd/yyyy");
+            cbCategory.Items.Add("Hot");
+            cbCategory.Items.Add("Warm");
+            cbCategory.Items.Add("Cold");
+        }
+
+        private void clearFields()
+        {
+            this.tbName.Text = string.Empty;
+            this.tbDate.Text = string.Empty;
+            this.cbCategory.SelectedIndex = 0;
+            this.cbAchieved.IsChecked = false;
         }
 
         private void btnSubmit_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            MainWindow.Instance.SetCurrentPanel(UseControls.BucketList);
+            string name = this.tbName.Text;
+            string date = this.tbDate.Text;
+            string category = this.cbCategory.SelectedValue.ToString();
+            bool achieved = this.cbAchieved.IsChecked.Value;
+
+            if (addEditModel.AddBucketListItem(name, date, category, achieved))
+            {
+                clearFields();
+                MainWindow.Instance.SetCurrentPanel(UseControls.BucketList);
+            }
+            else
+            {
+                // TODO - handle better
+                MessageBox.Show("Add Failed", "Login", MessageBoxButton.OK);
+                clearFields();
+            }
         }
 
         private void btnCancel_Click(object sender, System.Windows.RoutedEventArgs e)
